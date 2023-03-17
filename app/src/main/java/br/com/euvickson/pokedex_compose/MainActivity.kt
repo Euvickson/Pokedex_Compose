f9af4e1f1b9f05d38f56ce8459404f863f01437e
Euvickson <victor.eugennio@gmail.com>
2023-03-17 09:50:11 -0300

    Inittial Commit

---
+++ app/src/main/java/br/com/euvickson/pokedex_compose/MainActivity.kt
@@ -0,0 +1,43 @@
+package br.com.euvickson.pokedex_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.euvickson.pokedex_compose.ui.theme.Pokedex_ComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Pokedex_ComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Pokedex_ComposeTheme {
+        Greeting("Android")
+    }
+}
