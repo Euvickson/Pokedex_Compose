ae8fce042f5c402847d73a35b297413b37b4c70c
Euvickson <victor.eugennio@gmail.com>
2023-03-22 21:43:32 -0300

    Stilying the item

--- app/src/main/java/br/com/euvickson/pokedex_compose/ui/components/PokemonListItem.kt
+++ app/src/main/java/br/com/euvickson/pokedex_compose/ui/components/PokemonListItem.kt
@@ -1,12 +1,10 @@
 package br.com.euvickson.pokedex_compose.ui.components
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import br.com.euvickson.pokedex_compose.model.Pokemon
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
@@ -15,16 +13,26 @@ import com.bumptech.glide.integration.compose.GlideImage
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun PokemonListItem(pokemon: Pokemon) {
-    Column(Modifier.fillMaxWidth().height(300.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .height(300.dp), horizontalAlignment = Alignment.CenterHorizontally) {
         GlideImage(
             model = pokemon.imageUrl,
             contentDescription = "Pokemon Image",
             modifier = Modifier
         )
-        Row (modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
-            Text(text = "#${pokemon.formattedNumber}")
-            Text(text = pokemon.type1)
-            pokemon.type2?.let { Text(text = it) }
+        Row (modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+                Text(text = "#${pokemon.formattedNumber}")
+
+                Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                    Text(text = pokemon.type1)
+                    pokemon.type2?.let { Text(text = it) }
+                }
+
+            }
         }
     }
 }
