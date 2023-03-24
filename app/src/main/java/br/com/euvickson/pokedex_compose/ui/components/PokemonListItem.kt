289132da4bf0f7d6f27be4c36d580934c0a3471b
Euvickson <victor.eugennio@gmail.com>
2023-03-24 20:03:02 -0300

    Styled the item

--- app/src/main/java/br/com/euvickson/pokedex_compose/ui/components/PokemonListItem.kt
+++ app/src/main/java/br/com/euvickson/pokedex_compose/ui/components/PokemonListItem.kt
@@ -1,11 +1,15 @@
 package br.com.euvickson.pokedex_compose.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.euvickson.pokedex_compose.model.Pokemon
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
@@ -25,11 +29,13 @@ fun PokemonListItem(pokemon: Pokemon) {
         Row (modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
 
-                Text(text = "#${pokemon.formattedNumber}")
+                Text(text = "Nº #${pokemon.formattedNumber}")
+
+                Text(pokemon.name, fontWeight = FontWeight.Bold, fontSize = 24.sp)
 
                 Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-                    Text(text = pokemon.type1)
-                    pokemon.type2?.let { Text(text = it) }
+                    Text(text = pokemon.type1, modifier = Modifier.background(Color.LightGray).padding(horizontal = 32.dp))
+                    pokemon.type2?.let { Text(text = it, modifier = Modifier.background(Color.LightGray).padding(horizontal = 32.dp)) }
                 }
 
             }
