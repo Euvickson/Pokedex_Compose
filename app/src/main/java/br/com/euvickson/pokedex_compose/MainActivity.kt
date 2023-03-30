91eb78efa716c47c4c0200cdb377f3e8a69ced9c
Euvickson <victor.eugennio@gmail.com>
2023-03-30 20:59:34 -0300

    Refactoring the code.
    Introduced the navController and the NavHost to make the screen transitions

--- app/src/main/java/br/com/euvickson/pokedex_compose/MainActivity.kt
+++ app/src/main/java/br/com/euvickson/pokedex_compose/MainActivity.kt
@@ -1,7 +1,6 @@
 package br.com.euvickson.pokedex_compose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -13,6 +12,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -20,6 +20,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import br.com.euvickson.pokedex_compose.model.Pokemon
 import br.com.euvickson.pokedex_compose.ui.components.PokemonListItem
@@ -39,15 +42,21 @@ class MainActivity : ComponentActivity() {
                     val mainViewModel = viewModel(modelClass = MainViewModel::class.java)
                     val list by mainViewModel.listFlow.collectAsState()
                     val navController = rememberNavController()
-
-                    CreatePokemonList(list)
+                    NavHost(navController = navController, startDestination = "main") {
+                        composable("main") {
+                            CreatePokemonList(list, navController)
+                        }
+                        composable("description") {
+                            Text(text = "Trying to show in the screen")
+                        }
+                    }
                 }
             }
         }
     }
 
     @Composable
-    private fun CreatePokemonList(list: List<Pokemon>) {
+    private fun CreatePokemonList(list: List<Pokemon>, navController: NavHostController) {
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(8.dp),
             modifier = Modifier.fillMaxSize(),
@@ -65,7 +74,7 @@ class MainActivity : ComponentActivity() {
             } else {
                 items(list) { pokemon ->
                     PokemonListItem(pokemon = pokemon) {
-                        Log.i("TESTE", "CreatePokemonList: ${it}")
+                        navController.navigate("description")
                     }
                 }
             }
