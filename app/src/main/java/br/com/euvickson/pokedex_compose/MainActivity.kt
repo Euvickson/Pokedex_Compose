7fae4642e59c0cd1fc00fa98ecb655ca975d4742
Euvickson <victor.eugennio@gmail.com>
2023-04-01 22:35:12 -0300

    Started the pokemon detail Screen.
    Code Refactoting

--- app/src/main/java/br/com/euvickson/pokedex_compose/MainActivity.kt
+++ app/src/main/java/br/com/euvickson/pokedex_compose/MainActivity.kt
@@ -6,10 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -27,6 +27,8 @@ import br.com.euvickson.pokedex_compose.model.Pokemon
 import br.com.euvickson.pokedex_compose.ui.components.PokemonListItem
 import br.com.euvickson.pokedex_compose.ui.theme.Pokedex_ComposeTheme
 import br.com.euvickson.pokedex_compose.viewmodel.MainViewModel
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,10 +48,7 @@ class MainActivity : ComponentActivity() {
                         composable("main") {
                             ListScreen(pokemonList, navController)
                         }
-                        composable(
-                            "description/{id}",
-                            arguments = listOf(navArgument("id") { type = NavType.IntType })
-                        ) {
+                        composable("description/{id}", arguments = listOf(navArgument("id") { type = NavType.IntType })) {
                             val pokemon = pokemonList.find { pokemon -> pokemon.pokedexId == it.arguments?.getInt("id") }
                             DetailScreen(pokemon)
                         }
@@ -60,10 +59,26 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalGlideComposeApi::class)
     @Composable
-    private fun DetailScreen(pokemon: Pokemon?) {
-        Text(text = "Trying to show in the screen")
-        Text(text = "Pokemon id: ${pokemon?.name}")
+    private fun DetailScreen(nullablePokemon: Pokemon?) {
+        nullablePokemon?.let {pokemon ->
+
+            Column(modifier = Modifier.fillMaxSize()) {
+                Row(modifier = Modifier.padding(16.dp).fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                    Icon(Icons.Rounded.ArrowBack, contentDescription = "")
+                    Text(text = "Pokemon Nº #${pokemon.formattedNumber}")
+                    Icon(Icons.Rounded.ArrowForward, contentDescription = "")
+                }
+
+                GlideImage(
+                    model = pokemon.imageUrl,
+                    contentDescription = "Pokemon Image",
+                    modifier = Modifier.height(200.dp).width(200.dp)
+                )
+            }
+
+        } ?: Text(text = "An error occurred during the detail loading")
     }
 
     @Composable
